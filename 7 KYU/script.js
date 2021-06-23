5fb7b0e930e22e79fb862cbaf43b00f6eef50a0f
Nujsvart <kampfar@gmail.com>
2021-06-24 01:52:43 +0300

    7 KYU - (2)

--- 7 KYU/script.js
+++ 7 KYU/script.js
@@ -300,3 +300,32 @@ const validatePIN = pin => /^(\d{4}|\d{6})$/.test(pin);
 
 //******************************************** */
 
+/* Isograms
+
+An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+
+isIsogram("Dermatoglyphics") == true
+isIsogram("aba") == false
+isIsogram("moOse") == false // -- ignore letter case */
+
+const isIsogram = str => new Set(str.toLowerCase()).size === str.length;
+
+//******************************************** */
+
+/* Jaden Casing Strings
+
+Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010) and After Earth (2013). Jaden is also known for some of his philosophy that he delivers via Twitter. When writing on Twitter, he is known for almost always capitalizing every word. For simplicity, you'll have to capitalize each word, check out how contractions are expected to be in the example below.
+
+Your task is to convert strings to how they would be written by Jaden Smith. The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them.
+
+Example:
+
+Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
+Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real" */
+
+String.prototype.toJadenCase = function () {
+  return this.toLowerCase()
+    .split(" ")
+    .map((s, i) => s[0].toUpperCase() + s.slice(1))
+    .join(" ");
+};
